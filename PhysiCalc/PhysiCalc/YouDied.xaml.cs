3ea408ec2dcf83a11e2e013e2d00f1980dbabec8
Cesar Atencio <catencior@gmail.com>
2017-04-22 03:17:20 -0600

    Last touches

--- PhysiCalc/PhysiCalc/YouDied.xaml.cs
+++ PhysiCalc/PhysiCalc/YouDied.xaml.cs
@@ -23,5 +23,12 @@ namespace PhysiCalc
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainMenu = new MainWindow();
+            mainMenu.Show();
+            this.Close();
+        }
     }
 }
